3959e7d543ae76ddf0ccdf7a307442365aeb3fc6
Vetrivel21 <77160819+Vetrivel21@users.noreply.github.com>
2022-05-04 11:50:47 +0530

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from task.views import ReviewEmailView
-from app.views import send_notification
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('reviews/', ReviewEmailView.as_view(), name="reviews"),
-    path('send_notification', send_notification),
-    path('', include('app1.urls')),
+    path('', include('mail_app.urls')),
 ]
+    
